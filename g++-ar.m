8dac6d8510e210c29fbaa171b6d66eb7921f36f3
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-25 20:14:52 +0200

    Prepare libary creation script

---
+++ g++-ar.m
@@ -0,0 +1,99 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Copyright (C) 2022 Kevin Matthes
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License along
+%% with this program; if not, write to the Free Software Foundation, Inc.,
+%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%
+%%%%
+%%
+%%  FILE
+%%      ar-create.m
+%%
+%%  BRIEF
+%%      Create a static library from object files with `ar`.
+%%
+%%  AUTHOR
+%%      Kevin Matthes
+%%
+%%  COPYRIGHT
+%%      (C) 2022 Kevin Matthes.
+%%      This file is licensed GPL 2 as of June 1991.
+%%
+%%  DATE
+%%      2022
+%%
+%%  NOTE
+%%      See `LICENSE' for full license.
+%%      See `README.md' for project details.
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%
+%%
+%% Variables.
+%%
+%%%%
+
+% Software.
+archiver.args   = 'rsv';
+archiver.in     = '*.o';
+archiver.out    = ['lib'  '' '.a'];
+archiver.self   = 'ar';
+archiver.call   = [ archiver.self ' ' archiver.args ' ' archiver.out ' '     ...
+                    archiver.in                                              ...
+                  ];
+
+% Miscellaneous.
+misc.self   = 'ar-create.m';
+misc.banner = ['[ ' misc.self ' ] '];
+
+
+
+%%%%
+%%
+%% Build steps.
+%%
+%%%%
+
+% Begin build instruction.
+disp ([misc.banner 'Begin build instruction.']);
+
+
+
+% Clean outdated library.
+fprintf ([misc.banner 'Remove outdated version of ' archiver.out ' ... ']);
+
+if length (glob (archiver.out));
+    delete (archiver.out);
+end;
+
+disp ('Done.');
+
+
+
+% Call library creation tool.
+disp ([misc.banner 'Create library ' archiver.out ' ...']);
+
+disp (archiver.call);
+system (archiver.call);
+
+disp ([misc.banner 'Done.']);
+
+
+
+% End build instruction.
+disp ([misc.banner 'End build instruction.']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
