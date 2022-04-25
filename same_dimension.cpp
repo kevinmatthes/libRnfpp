e23c2a188dd2b09a6081635fd1b010a84867d267
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-25 22:12:49 +0200

    Prepare new method

---
+++ same_dimension.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2022 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The Rnfpp vector class.
+ * \copyright   (C) 2022 Kevin Matthes.
+ *              This file is licensed GPL 2 as of June 1991.
+ * \date        2022
+ * \file        assert_same_dimension.cpp
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ *
+ * This file implements the Rnfpp vector class.
+ */
+
+/******************************************************************************/
+
+/*
+ * Includes.
+ */
+
+// This project.
+#include "Rnfpp.hpp"
+
+
+
+/**
+ * \brief   Ensure two vectors to share their dimension.
+ * \param   other   The vector to test against.
+ * \throws  invalid_argument    In case the vectors are of different dimensions.
+ *
+ * This method tests whether this vector is of the same dimension as the given
+ * other one.  If not so, an exception will be thrown.
+ */
+
+inline void Rnfpp :: assert_same_dimension (const Rnfpp & other) const
+{
+    if (this -> components.size () != other.components.size ())
+        throw invalid_argument ("The given vectors have different dimensions!");
+
+    return;
+}
+
+/******************************************************************************/
