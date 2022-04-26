50c70c79abe8798179d2b8990d7016912cd3113a
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-26 21:08:49 +0200

    Bugfix

--- Rnfpp.cpp
+++ Rnfpp.cpp
@@ -80,7 +80,7 @@ inline Rnfpp :: Rnfpp (const dimension_t dimension)
 
 inline Rnfpp :: Rnfpp (const Rnfpp & rnfpp)
 {
-    Rnfpp (rnfpp.components);
+    Rnfpp (rnfpp.get_components ());
     return;
 }
 
