7aa056cd1345d043e656f61c0bea8019987ff57c
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-26 20:30:39 +0200

    Bugfix

--- Rnfpp.hpp
+++ Rnfpp.hpp
@@ -117,11 +117,12 @@ class Rnfpp
         EXPORT  vector <float> &    get_components  (void)  const noexcept;
         EXPORT  dimension_t         get_dimension   (void)  const noexcept;
 
-        EXPORT  Rnfpp   operator *  (const float    other)  const;
-        EXPORT  Rnfpp   operator +  (const Rnfpp &  other)  const;
-        EXPORT  Rnfpp   operator -  (const Rnfpp &  other)  const;
-        EXPORT  Rnfpp   operator /  (const float    other)  const;
-        EXPORT  bool    operator == (const Rnfpp &  other)  const;
+        EXPORT  Rnfpp   operator *  (const float        other)  const;
+        EXPORT  Rnfpp   operator +  (const Rnfpp &      other)  const;
+        EXPORT  Rnfpp   operator -  (const Rnfpp &      other)  const;
+        EXPORT  Rnfpp   operator /  (const float        other)  const;
+        EXPORT  bool    operator == (const Rnfpp &      other)  const;
+        EXPORT  float & operator [] (const dimension_t  other)  const;
 
         EXPORT  void    assert_non_zero         (const float    other)  const;
         EXPORT  void    assert_same_dimension   (const Rnfpp &  other)  const;
