ac24daee4ee9d274d3d4c042b04138085735d432
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-05-15 18:55:33 +0200

    Prepare next method

---
+++ assert_norm_non_zero.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2022 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The Rnfpp vector class.
+ * \copyright   (C) 2022 Kevin Matthes.
+ *              This file is licensed GPL 2 as of June 1991.
+ * \date        2022
+ * \file        assert_non_zero.cpp
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ *
+ * This file implements the Rnfpp vector class.
+ */
+
+/******************************************************************************/
+
+/*
+ * Includes.
+ */
+
+// This project.
+#include "Rnfpp.hpp"
+
+
+
+/**
+ * \brief   Ensure an object to be unequal to zero.
+ * \param   other   The object to test.
+ * \throws  invalid_argument    In case the object is zero.
+ *
+ * This method tests whether the given object is zero.  If so, an exception will
+ * be thrown.
+ */
+
+inline void Rnfpp :: assert_non_zero (const float other) const
+{
+    if (! abs (other))
+        throw invalid_argument ("A value unequal to zero is required!");
+
+    return;
+}
+
+/******************************************************************************/
