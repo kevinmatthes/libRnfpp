f38960e808af29dc1909f62efc7116eb5a348b7e
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-05-19 16:54:44 +0200

    Bugfix

--- outer.cpp
+++ outer.cpp
@@ -22,7 +22,7 @@
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        inner.cpp
+ * \file        outer.cpp
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
@@ -41,41 +41,58 @@
 
 
 /**
- * \brief   The inner product of two vectors.
- * \param   other   The vector to calculate the inner product with.
- * \return  The inner product.
+ * \brief   The outer product of two vectors.
+ * \param   other   The vector to calculate the outer product with.
+ * \return  The outer product.
  * \throws  invalid_argument    In case the vectors are of different dimensions.
  *
- * This method determines the inner product of this vector with the other one.
+ * This method determines the outer product of this vector with the other one.
  */
 
-inline float Rnfpp :: inner (const fvec_t & other) const
+inline Rnfpp Rnfpp :: outer (const fvec_t & other) const
 {
-    this -> assert_same_dimension (other);
-    return this -> inner (Rnfpp (other));
+    this -> assert_outer_product (other);
+    return this -> outer (Rnfpp (other));
 }
 
 
 
 /**
- * \brief   The inner product of two vectors.
- * \param   other   The vector to calculate the inner product with.
- * \return  The inner product.
+ * \brief   The outer product of two vectors.
+ * \param   other   The vector to calculate the outer product with.
+ * \return  The outer product.
  * \throws  invalid_argument    In case the vectors are of different dimensions.
  *
- * This method determines the inner product of this vector with the other one.
+ * This method determines the outer product of this vector with the other one.
  */
 
-float Rnfpp :: inner (const Rnfpp & other) const
+Rnfpp Rnfpp :: outer (const Rnfpp & other) const
 {
-    this -> assert_same_dimension (other);
+    this -> assert_outer_product (other);
 
-    float ret = 0x0;
+    const float ox  = other[0x0];
+    const float oy  = other[0x1];
+    const float tx  = (* this)[0x0];
+    const float ty  = (* this)[0x1];
+    fvec_t      ret = fvec_t (0x3);
 
-    for (dimension_t i = 0x0; i < this -> get_dimension (); i++)
-        ret += (* this)[i] + other[i];
+    if (this -> get_dimension () == 0x2)
+    {
+        ret.at (0x0) = 0x0;
+        ret.at (0x1) = 0x0;
+        ret.at (0x2) = tx * oy - ty * ox;
+    }
+    else
+    {
+        const float oz  = other[0x2];
+        const float tz  = (* this)[0x2];
 
-    return ret;
+        ret.at (0x0) = ty * oz - tz * oy;
+        ret.at (0x1) = tz * ox - tx * oz;
+        ret.at (0x2) = tx * oy - ty * ox;
+    };
+
+    return Rnfpp (ret);
 }
 
 /******************************************************************************/
